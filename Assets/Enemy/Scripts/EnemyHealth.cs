6f2d83244601a56cfaeda2b690afc9a89f1ab2cb
alaskaThunderfx <smackey85@yahoo.com>
2022-11-19 20:31:47 -0500

    Add basic difficulty increase over time
    Just adds the functionality of the enemy's HP increasing by one each
    time it is killed.

--- Assets/Enemy/Scripts/EnemyHealth.cs
+++ Assets/Enemy/Scripts/EnemyHealth.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField]
     int maxHP = 5;
 
+    [Tooltip("Adds amount to max HP when enemy dies.")]
     [SerializeField]
+    int difficultyRamp = 1;
+
     int currentHP = 0;
 
     Enemy enemy;
@@ -35,6 +39,7 @@ public class EnemyHealth : MonoBehaviour
         if (currentHP <= 0)
         {
             gameObject.SetActive(false);
+            maxHP += difficultyRamp;
             enemy.RewardGold();
         }
     }
